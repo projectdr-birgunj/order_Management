26b07cfae6efdedd0214ba13ebc0a8d9898224f4
raj-45 <rajnishkumarg123@gmail.com>
2024-09-08 15:46:55 +0530

    Replaced login.html with index.html

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -2,14 +2,14 @@
 // window.addEventListener('DOMContentLoaded', (event) => {
 //     if (localStorage.getItem("isLoggedIn") !== "true") {
 //         alert("Access denied. Please log in first.");
-//         window.location.href = "login.html";
+//         window.location.href = "index.html";
 //     }
 // });
 
 // function logout() {
 //     localStorage.removeItem("isLoggedIn");
 //     alert("You have been logged out.");
-//     window.location.href = "login.html";
+//     window.location.href = "index.html";
 // }
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js";
 import {
